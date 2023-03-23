3f6cbc0e1069f2b8f1b4a9c9a307f10722527d41
Brehima MALLE <36641801+MALLEDOSSONI@users.noreply.github.com>
2023-03-23 15:46:00 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -1,157 +1,147 @@
 <?php
-$films = [
+$datas = [
     [
-        '1', 'Impitoyable',
-        '1992',
-        'William Munny, tireur à la retraite du Old West, accepte à contrecœur un dernier emploi, avec l’aide de son ancien partenaire et d’un jeune homme.'
+        '#' => '1', 'film' => 'Impitoyable',
+        'annee' => '1992',
+        'description' => 'William Munny, tireur à la retraite du Old West, accepte à contrecœur un dernier emploi, avec l’aide de son ancien partenaire et d’un jeune homme.'
     ],
     [
-        '2', 'Reservoir Dogs ',
-        '1992',
-        'Lorsqu’un simple vol de bijoux tourne horriblement mal, les criminels survivants commencent à soupçonner que l’un d’entre eux est un informateur de la police.'
+        '#' => '2', 'film' => 'Reservoir Dogs ',
+        'annee' => '1992',
+        'description' => 'Lorsqu’un simple vol de bijoux tourne horriblement mal, les criminels survivants commencent à soupçonner que l’un d’entre eux est un informateur de la police.'
     ],
     [
-        '3', 'les evadés',
-        '1994',
-        'Deux hommes emprisonnés se lient pendant plusieurs années, trouvant réconfort et finalement rédemption par des actes de décence commune.'
+        '#' => '3', 'film' => 'les evadés',
+        'annee' => '1994',
+        'description' => 'Deux hommes emprisonnés se lient pendant plusieurs années, trouvant réconfort et finalement rédemption par des actes de décence commune.'
     ],
     [
-        '4', ' Fight Club ',
-        '1994',
-        'Un employé de bureau insomniaque et un savonnier diabolique forment un club de combat clandestin qui évolue vers quelque chose de beaucoup, beaucoup plus.'
+        '#' => '4', 'film' => ' Fight Club ',
+        'annee' => '1994',
+        'description' => 'Un employé de bureau insomniaque et un savonnier diabolique forment un club de combat clandestin qui évolue vers quelque chose de beaucoup, beaucoup plus.'
     ],
     [
-        '5', ' Forrest Gump',
-        '1994',
-        'The presidencies of Kennedy and Johnson, the events of Vietnam, Watergate, and other history unfold through the perspective of an Alabama man with'
+        '#' => '5', 'film' => ' Forrest Gump',
+        'annee' => '1994',
+        'description' => 'The presidencies of Kennedy and Johnson, the events of Vietnam, Watergate, and other history unfold through the perspective of an Alabama man with'
     ],
     [
-        '6', ' Léon',
-        '1994',
-        'Mathilda, une jeune fille de 12 ans, est recueillie à contrecœur par Léon, un assassin professionnel, après le meurtre de sa famille. Léon et Mathilda forment une relation inhabituelle, alors qu’elle devient sa protégée et apprend l’assassin.'
+        '#' => '6', 'film' => ' Léon',
+        'annee' => '1994',
+        'description' => 'Mathilda, une jeune fille de 12 ans, est recueillie à contrecœur par Léon, un assassin professionnel, après le meurtre de sa famille. Léon et Mathilda forment une relation inhabituelle, alors qu’elle devient sa protégée et apprend l’assassin.'
     ],
     [
-        '7', 'La haine',
-        '1995',
-        '24 hours in the lives of three young men in the French suburbs the day after a violent riot'
+        '#' => '7', 'film' => 'La haine',
+        'annee' => '1995',
+        'description' => '24 hours in the lives of three young men in the French suburbs the day after a violent riot'
     ],
     [
-        '8', 'Seven',
-        '1995',
-        'Two detectives, a rookie and a veteran, hunt a serial killer who uses the seven deadly sins as his motives'
+        '#' => '8', 'film' => 'Seven',
+        'annee' => '1995',
+        'description' => 'Two detectives, a rookie and a veteran, hunt a serial killer who uses the seven deadly sins as his motives'
     ],
     [
-        '9', ' Usual Suspects',
-        '1995',
-        'A sole survivor tells of the twisty events leading up to a horrific gun battle on a boat, which began when five criminals met at a seemingly random police lineup'
+        '#' => '9', 'film' => ' Usual Suspects',
+        'annee' => '1995',
+        'description' => 'A sole survivor tells of the twisty events leading up to a horrific gun battle on a boat, which began when five criminals met at a seemingly random police lineup'
     ],
     [
-        '10', 'La vie est belle ',
-        '1997',
-        'When an open-minded Jewish librarian and his son become victims of the Holocaust, he uses a perfect mixture of will, humor, and imagination to protect his son from the dangers around their camp'
+        '#' => '10', 'film' => 'La vie est belle ',
+        'annee' => '1997',
+        'description' => 'When an open-minded Jewish librarian and his son become victims of the Holocaust, he uses a perfect mixture of will, humor, and imagination to protect his son from the dangers around their camp'
     ],
     [
-        '11', '  Will Hunting',
-        '1997',
-        'Will Hunting, a janitor at M.I.T., has a gift for mathematics, but needs help from a psychologist to find direction in his life.'
+        '#' => '11', 'film' => '  Will Hunting',
+        'annee' => '1997',
+        'description' => 'Will Hunting, a janitor at M.I.T., has a gift for mathematics, but needs help from a psychologist to find direction in his life.'
     ],
     [
-        '12', 'Avengers: Infinity War',
-        '2018',
-        'The Avengers and their allies must be willing to sacrifice all in an attempt to defeat the powerful Thanos before his blitz of devastation and ruin puts an end to the universe'
+        '#' => '12', 'film' => 'Avengers: Infinity War',
+        'annee' => '2018',
+        'description' => 'The Avengers and their allies must be willing to sacrifice all in an attempt to defeat the powerful Thanos before his blitz of devastation and ruin puts an end to the universe'
     ],
     [
-        '13', 'Spider-Man: New Generation',
-        '2018',
-        'Teen Miles Morales becomes Spider-Man of his reality, crossing his path with five counterparts from other dimensions to stop a threat for all realities'
+        '#' => '13', 'film' => 'Spider-Man: New Generation',
+        'annee' => '2018',
+        'description' => 'Teen Miles Morales becomes Spider-Man of his reality, crossing his path with five counterparts from other dimensions to stop a threat for all realities'
     ],
     [
-        '14', 'Capharnaum',
-        '2018',
-        'While serving a five-year sentence for a violent crime, a 12-year-old boy sues his parents for neglect'
+        '#' => '14', 'film' => 'Capharnaum',
+        'annee' => '2018',
+        'description' => 'While serving a five-year sentence for a violent crime, a 12-year-old boy sues his parents for neglect'
     ],
     [
-        '15', 'Dangal',
-        '2016',
-        'Former wrestler Mahavir Singh Phogat and his two wrestler daughters struggle towards glory at the Commonwealth Games in the face of societal oppression'
+        '#' => '15', 'film' => 'Dangal',
+        'annee' => '2016',
+        'description' => 'Former wrestler Mahavir Singh Phogat and his two wrestler daughters struggle towards glory at the Commonwealth Games in the face of societal oppression'
     ],
     [
-        '16', 'Tu ne tueras point ',
-        '2016',
-        'World War II American Army Medic Desmond T. Doss, who served during the Battle of Okinawa, refuses to kill people, and becomes the first man in American history to receive the Medal of Honor without firing a shot'
+        '#' => '16', 'film' => 'Tu ne tueras point ',
+        'annee' => '2016',
+        'description' => 'World War II American Army Medic Desmond T. Doss, who served during the Battle of Okinawa, refuses to kill people, and becomes the first man in American history to receive the Medal of Honor without firing a shot'
     ],
     [
-        '17', 'Mademoiselle',
-        '2016',
-        'A woman is hired as a handmaiden to a Japanese heiress, but secretly she is involved in a plot to defraud her'
+        '#' => '17', 'film' => 'Mademoiselle',
+        'annee' => '2016',
+        'description' => 'A woman is hired as a handmaiden to a Japanese heiress, but secretly she is involved in a plot to defraud her'
     ],
     [
-        '18', 'Les Émotifs anonymes',
-        '2010',
-        "Deux hommes emprisonnés se lient pendant plusieurs années, trouvant réconfort et finalement rédemption par des actes de décence commune."
+        '#' => '18', 'film' => 'Les Émotifs anonymes',
+        'annee' => '2010',
+        'description' => "Deux hommes emprisonnés se lient pendant plusieurs années, trouvant réconfort et finalement rédemption par des actes de décence commune."
     ],
     [
-        '19', 'Killing Bono',
-        '2011',
-        'Deux hommes emprisonnés se lient pendant plusieurs années, trouvant réconfort et finalement rédemption par des actes de décence commune.'
+        '#' => '19', 'film' => 'Killing Bono',
+        'annee' => '2011',
+        'description' => 'Deux hommes emprisonnés se lient pendant plusieurs années, trouvant réconfort et finalement rédemption par des actes de décence commune.'
     ],
     [
-        '20', ' Robin des Bois - Prince des voleurs',
-        '1991',
-        "Deux hommes emprisonnés se lient pendant plusieurs années, trouvant réconfort et finalement rédemption par des actes de décence commune."
+        '#' => '20', 'film' => ' Robin des Bois - Prince des voleurs',
+        'annee' => '1991',
+        'description' => "Deux hommes emprisonnés se lient pendant plusieurs années, trouvant réconfort et finalement rédemption par des actes de décence commune."
     ],
     [
-        '21', 'À la recherche du bonheur',
-        '2006',
-        "Deux hommes emprisonnés se lient pendant plusieurs années, trouvant réconfort et finalement rédemption par des actes de décence commune."
+        '#' => '21', 'film' => 'À la recherche du bonheur',
+        'annee' => '2006',
+        'description' => "Deux hommes emprisonnés se lient pendant plusieurs années, trouvant réconfort et finalement rédemption par des actes de décence commune."
     ],
     [
-        '22', 'Le Professionnel ',
-        '1981',
-        'Fabien73 a mis 8/10 et a écrit une critique.'
+        '#' => '22', 'film' => 'Le Professionnel ',
+        'annee' => '1981',
+        'description' => 'Fabien73 a mis 8/10 et a écrit une critique.'
     ],
     [
-        '23', 'les evadés',
-        '1994',
-        'Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency.'
+        '#' => '23', 'film' => 'les evadés',
+        'annee' => '1994',
+        'description' => 'Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency.'
     ],
     [
-        '24', ' Fight Club ',
-        '1994',
-        'An insomniac office worker and a devil-may-care soapmaker form an underground fight club that evolves into something much, much more.'
+        '#' => '24', 'film' => ' Fight Club ',
+        'annee' => '1994',
+        'description' => 'An insomniac office worker and a devil-may-care soapmaker form an underground fight club that evolves into something much, much more.'
     ],
     [
-        '25', ' Forrest Gump',
-        '1994',
-        'The presidencies of Kennedy and Johnson, the events of Vietnam, Watergate, and other history unfold through the perspective of an Alabama man with'
+        '#' => '25', 'film' => ' Forrest Gump',
+        'annee' => '1994',
+        'description' => 'The presidencies of Kennedy and Johnson, the events of Vietnam, Watergate, and other history unfold through the perspective of an Alabama man with'
     ],
     [
-        '26', ' Léon',
-        '1994',
-        'Mathilda, a 12-year-old girl, is reluctantly taken in by Léon, a professional assassin, after her family is murdered. Léon and Mathilda form an unusual relationship, as she becomes his protégée and learns the assassin'
+        '#' => '26', 'film' => ' Léon',
+        'annee' => '1994',
+        'description' => 'Mathilda, a 12-year-old girl, is reluctantly taken in by Léon, a professional assassin, after her family is murdered. Léon and Mathilda form an unusual relationship, as she becomes his protégée and learns the assassin'
     ],
     [
-        '27', 'La haine',
-        '1995',
-        '24 hours in the lives of three young men in the French suburbs the day after a violent riot'
+        '#' => '27', 'film' => 'La haine',
+        'annee' => '1995',
+        'description' => '24 hours in the lives of three young men in the French suburbs the day after a violent riot'
     ],
     [
-        '28', 'Seven',
-        '1995',
-        'Two detectives, a rookie and a veteran, hunt a serial killer who uses the seven deadly sins as his motives'
+        '#' => '28', 'film' => 'Seven',
+        'annee' => '1995',
+        'description' => 'Two detectives, a rookie and a veteran, hunt a serial killer who uses the seven deadly sins as his motives'
     ]
 ];
 $search = $_GET['search'];
-$films_filtered = array_filter($films, function ($film) {
-    global $films;
-    global $search;
-    for ($i = 0; $i <= count($films); $i++) {
-        if ($film[$i] === $search) {
-        }
-    }
-    return $film[$i];
-});
-var_dump($films_filtered);
 ?>
 <!doctype html>
 <html lang="fr">
@@ -160,10 +150,8 @@ var_dump($films_filtered);
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Application de Recherche film</title>
-    <link rel="stylesheet" href="style.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
-
 </head>
 
 <body>
@@ -171,7 +159,8 @@ var_dump($films_filtered);
         <div class="container-fluid">
             <a class="navbar-brand">FILM</a>
             <form class="d-flex" role="search" action="index.php" method="GET">
-                <input class="form-control me-2" name="search" type="search" placeholder="Search" aria-label="Search">
+                <input class="form-control me-2" name="search" type="search" placeholder="Search" aria-label="Search"
+                    value="">
                 <button class="btn btn-outline-success" type="submit">Search</button>
             </form>
         </div>
@@ -179,7 +168,7 @@ var_dump($films_filtered);
     <div class="container">
         <table class="table table-striped">
             <thead>
-                <tr>
+                <tr>'
                     <th>#</th>
                     <th>Movie</th>
                     <th>Year</th>
@@ -187,13 +176,31 @@ var_dump($films_filtered);
                 </tr>
             </thead>
             <tbody>
-                <?php foreach ($films as $film) { ?>
-                <tr>
-                    <?php foreach ($film as $field) { ?>
-                    <td><?php echo $field; ?></td>
-                    <?php } ?>
-                </tr>
-                <?php } ?>
+
+                <?php
+                if (isset($search)) {
+
+                    foreach ($datas as $data) {
+                        echo ' <tr>';
+                        if (empty($search)) {
+
+                            echo '<td>' . $data['#'] . '</td>';
+                            echo '<td>' . $data['film'] . '</td>';
+                            echo '<td>' . $data['annee'] . '</td>';
+                            echo '<td>' . $data['description'] . '</td>';
+                        } elseif ($data['annee'] === $search) {
+                            echo '<td>' . $data['#'] . '</td>';
+                            echo '<td>' . $data['film'] . '</td>';
+                            echo '<td>' . $data['annee'] . '</td>';
+                            echo '<td>' . $data['description'] . '</td>';
+                        }
+                        echo ' </tr>';
+                    }
+                    echo "\n";
+                }
+
+                ?>
+
             </tbody>
         </table>
     </div>
